fe95e9e381ba932737181b79f86c890b9b356456
WouterVH <WouterVH@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-04-01 13:08:05 +0000

    add rebuild_i18n-script

---
+++ plone/app/changeownership/rebuild_i18n.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#for productname, take the lowercased package-name without toplevel-namespace
+#Products.Foo  --> foo.po
+PRODUCTNAME=plone.app.changeownership   #name po-file
+
+# use lowercase package-name for i18ndomain
+I18NDOMAIN=$PRODUCTNAME  # not the same!
+
+
+# Synchronise the .pot with the templates.
+i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --create ${I18NDOMAIN} .
+
+# Synchronise the resulting .pot with the .po files
+i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}.po
+
+# Zope3 is lazy so we have to compile the po files ourselves (Plone3.0)
+# automatic compilation is fixed since plone3.1
+#for lang in $(find locales -mindepth 1 -maxdepth 1 -type d); do
+#    if test -d $lang/LC_MESSAGES; then
+#        msgfmt -o $lang/LC_MESSAGES/${PRODUCTNAME}.mo $lang/LC_MESSAGES/${PRODUCTNAME}.po
+#    fi
+#done

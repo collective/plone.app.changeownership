7d889ed6c2d6a1f737c070a8be2ec41f16684fb6
Luca Fabbri <luca@keul.it>
2014-03-03 11:46:23 +0100

    few tests, just for main feature

--- plone/app/changeownership/tests/test_change_ownership.py
+++ plone/app/changeownership/tests/test_change_ownership.py
@@ -5,6 +5,7 @@ import unittest
 from zope.component import getMultiAdapter
 
 from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_ID
 from plone.app.testing import login
 
 from plone.app.changeownership.testing import OWNERSHIP_INTEGRATION_TESTING
@@ -17,8 +18,38 @@ class ChangeOWnershipTestCase(unittest.TestCase):
     def setUp(self):
         portal = self.layer['portal']
         login(portal, TEST_USER_NAME)
+        self._createContent()
 
-    def test_xxx(self):
-        """Be sure that we have no problems with non-ASCII chars"""
+    def _createContent(self):
+        portal = self.layer['portal']
+        portal.invokeFactory(type_name="Document", id='page', title="New document")
+
+    def test_fake_oldusers(self):
         portal = self.layer['portal']
         request = self.layer['request']
+        request.form['oldowners'] = ['foo']
+        request.form['newowner'] = 'user'
+        request.form['submit'] = '1'
+        view = getMultiAdapter((portal.page, request), name=u"change-owner")
+        view.change_owner()
+        self.assertTrue(portal.page.Creator(), TEST_USER_ID)
+
+    def test_fake_new_user(self):
+        portal = self.layer['portal']
+        request = self.layer['request']
+        request.form['oldowners'] = [TEST_USER_ID]
+        request.form['newowner'] = 'imnothere'
+        request.form['submit'] = '1'
+        view = getMultiAdapter((portal.page, request), name=u"change-owner")
+        self.assertRaises(KeyError, view.change_owner)
+
+    def test_change_Creator(self):
+        portal = self.layer['portal']
+        request = self.layer['request']
+        request.form['oldowners'] = [TEST_USER_ID]
+        request.form['newowner'] = 'user'
+        request.form['submit'] = '1'
+        view = getMultiAdapter((portal.page, request), name=u"change-owner")
+        view.change_owner()
+        self.assertEqual(portal.page.Creator(), 'user')
+

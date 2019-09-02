639e7c6cdf7e1020dbf2c05bacbf69ee8871c15c
Tom Gross <itconsense@gmail.com>
2019-09-02 22:41:24 +0200

    Fix reportings of pyroma

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='plone.app.changeownership',
       description="Change Plone objects ownership",
       long_description=open("README.rst").read() + "\n" +
                        open("CHANGES.rst").read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from https://pypi.org/classifiers/
       classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 3.3",
@@ -25,6 +25,8 @@ setup(name='plone.app.changeownership',
         "Framework :: Plone :: 5.1",
         "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
